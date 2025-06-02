711f90d716cbc5c81111d4ae64070c34062ddf24
DavidCarvajal122 <david.carvajal.delrosario@udla.edu.ec>
2025-06-02 07:13:42 -0500

    Agregar archivos de proyecto.

---
+++ CarvajalDExamen/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CarvajalDExamen
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CarvajalDExamen/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CarvajalDExamen
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
